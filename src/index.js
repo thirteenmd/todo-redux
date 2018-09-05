b837857b2c472d20a9cda197910690d175102206
thirteenmd <andreacojocaru@gmail.com>
2018-09-05 15:54:49 +0300

    configured for reduxdevtools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers'
 import App from './components/App'
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer)
+const middleware = [thunk];
+
+const initialState = {};
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ))
 
 render(
   <Provider store={store}>
